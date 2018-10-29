f148c96f23db981176fa18c17472122d75f8829e
24aprel <44571501+24aprel@users.noreply.github.com>
2018-10-29 16:10:16 +0300

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -37,6 +37,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline="January 01 2018 00:00:00 GMT+0300"; //for Ukraine
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
+var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
 initializeClock('counter-time', deadline);
